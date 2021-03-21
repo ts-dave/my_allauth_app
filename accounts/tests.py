d0f21242ad273f40c0250d000d08c50f5808dabe
tsdave <tsatsudave@gmail.com>
2021-03-21 23:15:44 +0000

    configure travis ci

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from accounts.models import User
+from django.test import Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class BaseTest(TestCase):
+
+    def setUp(self) -> None:
+        self.register_url = reverse()
+        self.login_url = reverse()
+        self.user = {
+            'email': 'testuser@gmail.com',
+            'username': 'myusername',
+            'password1': 'mypassword',
+            'password2': 'mypassword',
+        }
+        return super().setUp()
